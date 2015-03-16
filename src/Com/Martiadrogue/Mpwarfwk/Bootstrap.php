3447d52f964492533356c1c4f05c479d7110dd63
marti adrogue <marti.adrogue@gmail.com>
2015-03-16 16:56:00 +0100

    Add Bootstrap for production.

---
+++ src/Com/Martiadrogue/Mpwarfwk/Bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+namespace Com\Martiadrogue\Mpwarfwk;
+
+use Com\Martiadrogue\Mpwarfwk\Connection\Http\Response;
+use Com\Martiadrogue\Mpwarfwk\Connection\Http\Request;
+use Com\Martiadrogue\Mpwarfwk\Routing\Router;
+
+/**
+ *
+ */
+class Bootstrap
+{
+    public function boot()
+    {
+        $request = Request::createFromGlobals();
+        $response = $this->handle($request);
+        $response->send();
+    }
+
+    private function handle(Request $request)
+    {
+        $response = new Response();
+        $this->mapingRoutes($request);
+
+        return $response;
+    }
+
+    private function mapingRoutes(Request $request)
+    {
+        $router = new Router($request);
+        $router->add('/', 'HomeController()');
+        $router->add('/about', 'AboutController()');
+        $router->add('/test', 'TestController()');
+        $router->add('/test/list', 'TestController::list()');
+        $router->add('/test/{id}', 'TestController::show()');
+        $router->add('/contact', 'ContactController()');
+        $router->submit();
+    }
+}
