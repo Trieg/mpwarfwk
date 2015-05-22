39e00213fafc5c42d534ddd0538c0455201027ef
marti adrogue <marti.adrogue@gmail.com>
2015-05-22 20:46:04 +0200

    Selective attachment of DebugBar.

--- src/Com/Martiadrogue/Mpwarfwk/BootstrapDev.php
+++ src/Com/Martiadrogue/Mpwarfwk/BootstrapDev.php
@@ -4,6 +4,9 @@ namespace Com\Martiadrogue\Mpwarfwk;
 
 use Com\Martiadrogue\Mpwarfwk\Connection\BaseRequest;
 use Com\Martiadrogue\Mpwarfwk\Debug\DebugBar;
+use Com\Martiadrogue\Mpwarfwk\Connection\Http\HtmlResponse;
+use Com\Martiadrogue\Mpwarfwk\Connection\Responsible;
+
 /**
  *
  */
@@ -19,8 +22,16 @@ class BootstrapDev extends Bootstrap
 
     public function boot(BaseRequest $request)
     {
-        parent::boot($request);
-        $this->debugBar->regist();
-        echo $this->debugBar->render();
+        $response = parent::handle($request);
+        $response->send();
+        $this->attachDebugBar($response);
+    }
+
+    private function attachDebugBar(Responsible $response)
+    {
+        if ($response instanceof HtmlResponse) {
+            $this->debugBar->regist();
+            echo $this->debugBar->render();
+        }
     }
 }
