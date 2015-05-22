e324971881564bed2ee684d7298c423ddf62e88c
marti adrogue <marti.adrogue@gmail.com>
2015-05-22 09:54:50 +0200

    Measure execution time of php scripts in Development Environment.

--- src/Com/Martiadrogue/Mpwarfwk/BootstrapDev.php
+++ src/Com/Martiadrogue/Mpwarfwk/BootstrapDev.php
@@ -2,13 +2,25 @@
 
 namespace Com\Martiadrogue\Mpwarfwk;
 
+use Com\Martiadrogue\Mpwarfwk\Connection\BaseRequest;
+
 /**
  *
  */
 class BootstrapDev extends Bootstrap
 {
+    private $timeStart;
+
     public function __construct()
     {
         Debug::enable();
+        $this->start = microtime(true);
+    }
+
+    public function boot(BaseRequest $request)
+    {
+        parent::boot($request);
+        $timeElapsedSeconds = microtime(true) - $this->timeStart;
+        echo 'Execution time: ' . $timeElapsedSeconds.' seconds';
     }
 }
