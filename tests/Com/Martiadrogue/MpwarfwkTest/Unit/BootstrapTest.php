ffba8cf095ff55850d949476865cb7e70d4d477d
marti adrogue <marti.adrogue@gmail.com>
2015-06-03 12:15:44 +0200

    Add some tests.

--- tests/Com/Martiadrogue/MpwarfwkTest/Unit/BootstrapTest.php
+++ tests/Com/Martiadrogue/MpwarfwkTest/Unit/BootstrapTest.php
@@ -3,8 +3,13 @@
 namespace Com\Martiadrogue\MpwarfwkTest;
 
 use Com\Martiadrogue\Mpwarfwk\Bootstrap;
+use Com\Martiadrogue\Mpwarfwk\Connection\Http\Request;
 
 class BootstrapTest extends \PHPUnit_Framework_TestCase
 {
-
+    /** @test */
+    public function shouldReturnBaseRequest()
+    {
+        new Bootstrap();
+    }
 }
