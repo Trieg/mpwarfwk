4132b9d59a0be1214997a88be3e883bfce9424b7
marti adrogue <marti.adrogue@gmail.com>
2015-06-08 13:37:49 +0200

    Add new hash for passwords.

---
+++ src/Com/Martiadrogue/Mpwarfwk/Security/Hashes/Bcrypter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Com\Martiadrogue\Mpwarfwk\Security\Hashes;
+
+class Bcrypter
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function hash($password)
+    {
+        $options = [
+                'cost' => 11,
+                'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+            ];
+        return password_hash($password, PASSWORD_BCRYPT, $options);
+    }
+
+    public function verifyPassword($password, $hash)
+    {
+        return password_verify($password, $hash);
+    }
+}
