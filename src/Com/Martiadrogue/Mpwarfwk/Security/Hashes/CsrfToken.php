9f9c6e6c7e02dd4510de82d865b9193d0d280585
marti adrogue <marti.adrogue@gmail.com>
2015-06-20 18:09:09 +0200

    Remove responsability to mange Session from class CsrfToken.

--- src/Com/Martiadrogue/Mpwarfwk/Security/Hashes/CsrfToken.php
+++ src/Com/Martiadrogue/Mpwarfwk/Security/Hashes/CsrfToken.php
@@ -2,33 +2,16 @@
 
 namespace Com\Martiadrogue\Mpwarfwk\Security\Hashes;
 
-use Com\Martiadrogue\Mpwarfwk\Connection\Http\Session;
-use Com\Martiadrogue\Mpwarfwk\Exception\TokenMismatchException;
-
 class CsrfToken
 {
-    private $session;
-
-    public function __construct(Session $session)
+    public function __construct()
     {
-        $this->session = $session;
     }
 
     public function generate()
     {
         $hash = openssl_random_pseudo_bytes(32);
-        $hashEncoded = base64_encode($hash);
-        $this->session->setValue('csrf-token', $hashEncoded);
 
-        return $hashEncoded;
-    }
-
-    public function check($token)
-    {
-        if ($token !== $this->session->getData('csrf-token')) {
-            throw new TokenMismatchException();
-        }
-
-        $this->session->unsetData('csrf-token');
+        return base64_encode($hash);
     }
 }
